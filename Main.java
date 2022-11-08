c148fd88f62360bfb44b5584ec7bd09ca89145f8
koharu <k.watanabe.sys22@morijyobi.ac.jp>
2022-11-08 10:13:27 +0900

    BMI»èAvðüÇ

--- Main.java
+++ Main.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 	Scanner stdIn = new Scanner(System.in);
-	System.out.print("・身長(cm)を入力してください：");
+	System.out.print("・一人目の身長(cm)を入力してください：");
     	double height = stdIn.nextDouble(); // 身長を入力
-    	System.out.print("・体重(Kg)を入力してください：");
+    	System.out.print("・一人目の体重(Kg)を入力してください：");
     	double weight = stdIn.nextDouble(); // 体重を入力
 	double bmi = weight / Math.pow((height/100), 2);
-	System.out.println("BMI値は" + (((double)Math.round(bmi*100))/100) + "です。");
+	 System.out.print("・二人目の身長(cm)を入力してください：");
+        double height = stdIn.nextDouble(); // 身長を入力
+        System.out.print("・二人目の体重(Kg)を入力してください：");
+        double weight = stdIn.nextDouble(); // 体重を入力
+        double bmi = weight / Math.pow((height/100), 2);
+	System.out.println("一人目のBMI値は" + (((double)Math.round(bmi*100))/100) + "です。");
+	System.out.println("二人目のBMI値は" + (((double)Math.round(bmi*100))/100) + "です。");
 	}
 }
 
